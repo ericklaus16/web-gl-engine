900199f216c9d3e50f128ae7c1dcf662d7846913
Eric Klaus <ericklaus16@gmail.com>
2025-06-03 16:13:46 -0300

    Ajusta a estrutura do HTML, remove link duplicado para o CSS e define largura e altura do canvas no script.js.

--- js/script.js
+++ js/script.js
@@ -8,6 +8,9 @@ main();
 
 function main() {
     const canvas = document.querySelector("#canvas")
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+    
     const gl = canvas.getContext("webgl")
 
     if(!gl) {
